c587dbac1d0e909e105166eaff2521912bc5519a
andy <95575243@qq.com>
2023-03-31 18:15:20 +0800

    [refactor] 变更路由

--- api/routes/txs.routers.js
+++ api/routes/txs.routers.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
-const findByAddress = require('../service/ApiTxsFindByAddress.service')
+const { findByAddress, findByAddressValidator } = require('../service/ApiTxsFindByAddress.service')
 const router = Router();
 
-router.get('/txs', findByAddress);
+router.get('/txs', findByAddressValidator, findByAddress);
 
 module.exports = router
