9221b2c6d3ee998e9af19dc49d2257a9384b9c51
andy <95575243@qq.com>
2023-03-31 19:22:11 +0800

    [feat] 新增 etherscan 表 分页查询 函数

--- api/dao/EtherscanDao.js
+++ api/dao/EtherscanDao.js
@@ -4,6 +4,20 @@ class EtherscanDao extends IDataBase {
     constructor(databasename) {
         super(databasename);
     }
+
+    async paginationQuery(params, page, pageSize) {
+        try {
+            const list = await this.db
+                .cursor(params)
+                .skip((page - 1) * pageSize)
+                .limit(pageSize)
+                .exec();
+            return list
+        } catch (e) {
+            console.log(`表 ${this.databasename} | 函数 paginationQuery 分页查询出错 ${e.stack}`);
+            return []
+        }
+    }
 }
 
 module.exports = new EtherscanDao("etherscan");
